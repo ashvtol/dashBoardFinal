030f80bbaddccb55e23ba59b8732d46ea160a0f5
Ashish Kumar <ash.vtol@gmail.com>
2020-05-19 14:36:31 -0400

    Updates

---
+++ precache-manifest.9b1913ab840216a6c758618389b49b8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798b8b156ec86ac3070022a5afeb776f",
+    "url": "/dashBoardFinal/index.html"
+  },
+  {
+    "revision": "ba990440a1e9a4d76a8b",
+    "url": "/dashBoardFinal/static/css/main.6701cf04.chunk.css"
+  },
+  {
+    "revision": "a3e9f9a8f623900b11a1",
+    "url": "/dashBoardFinal/static/js/2.77ace540.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/dashBoardFinal/static/js/2.77ace540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba990440a1e9a4d76a8b",
+    "url": "/dashBoardFinal/static/js/main.6b3820fc.chunk.js"
+  },
+  {
+    "revision": "3ccb1daf30d742558646",
+    "url": "/dashBoardFinal/static/js/runtime-main.9ce58a03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashBoardFinal/static/media/logo.5d5d9eef.svg"
+  }
+]);
